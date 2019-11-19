0dd1407f2f9ef008c3ff88516f65ea56610110b7
minz27 <mreenav@gmail.com>
2019-11-19 10:19:31 +0530

    mapped neo4j port to 8484

--- angular-frontend/src/app/getlist.service.ts
+++ angular-frontend/src/app/getlist.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,6 @@ export class GetlistService {
   {
     console.log(pass);
     let data = {"email" : email ,"mobileNum" : mobile, "password" : pass };
-    return this.http.post('http://localhost:8080/api/v1/save', JSON.stringify(data),{headers:{'Content-Type': 'application/json'}})
+    return this.http.post(environment.apiUrl + ':8080/api/v1/save', JSON.stringify(data),{headers:{'Content-Type': 'application/json'}})
   }
 }
--- angular-frontend/src/environments/environment.ts
+++ angular-frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://15.206.105.26/'
+  apiUrl: 'http://15.206.105.26'
 };
 
 /*
