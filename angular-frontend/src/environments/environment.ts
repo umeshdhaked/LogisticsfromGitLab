41d8cf7096efe4c2fad67bfe8ae20e0cae1888f4
rajender97 <nayak3539@gmail.com>
2019-11-29 09:43:49 +0530

    vehicle-companey

--- angular-frontend/src/environments/environment.ts
+++ angular-frontend/src/environments/environment.ts
@@ -4,7 +4,10 @@
 
 export const environment = {
   production: false,
- apiUrl: 'http://15.206.105.26',
+//  apiUrl: 'http://15.206.105.26',
+
+ apiUrl:'http://172.23.239.96',
+
 
   //  apiUrl: 'http://localhost'
 };
