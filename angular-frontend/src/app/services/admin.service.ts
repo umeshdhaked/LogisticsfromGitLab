fe6604a6998ead8e4b5b9fee22914ae4a699d4be
minz27 <mreenav@gmail.com>
2019-12-13 12:27:01 +0530

    error in admin url

--- angular-frontend/src/app/services/admin.service.ts
+++ angular-frontend/src/app/services/admin.service.ts
@@ -17,7 +17,7 @@ export class AdminService {
 
 
    postnewcontactus(data: any)  {
-    const url = environment.apiUrl + ':9086//savenewcontactusrequest';
+    const url = environment.apiUrl + ':9086/savenewcontactusrequest';
     this.http.post(url, data);
 
    }
