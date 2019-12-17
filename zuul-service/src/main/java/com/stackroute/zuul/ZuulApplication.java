93cc1138cb0598e7462dc8c215917d56ecb9e53c
minz27 <mreenav@gmail.com>
2019-12-17 19:25:21 +0530

    added zuul profile

--- zuul-service/src/main/java/com/stackroute/zuul/ZuulApplication.java
+++ zuul-service/src/main/java/com/stackroute/zuul/ZuulApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @EnableZuulProxy
-@CrossOrigin("*")
+// @CrossOrigin("*")
 public class ZuulApplication {
 
     public static void main(String[] args) {
